2bc5f669a0be5b301c5476a60290c82333e2f8af
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-22 21:59:56 +0500

    Printing the Length of a String

---
+++ Calculating-Length-of-a-String.py
@@ -0,0 +1,9 @@
+# By simply printing the length of a string
+print(len("Jawad"))
+# output: 5
+
+
+# By taking Input from the user and Printing Length of a String in a single line of code
+
+
+print(len(input("What's Your Name? ")))
