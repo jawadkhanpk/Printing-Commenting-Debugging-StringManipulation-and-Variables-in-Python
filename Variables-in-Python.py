c30c19911e457367df6e3721946eb98c3d92fb0b
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-22 22:10:09 +0500

    Variables in Python

---
+++ Variables-in-Python.py
@@ -0,0 +1,11 @@
+# Printing string stored inside name variable
+name = "Jawad Khan"
+print(name)
+
+# Taking input from user, storing it inside name variable and printing it on screen
+name = input("Whats Your Name? ")
+print(name)
+
+# Taking input from user, storing it inside name variable and printing length of a name
+name = input("What's Your Name? ")
+print(len(name))
