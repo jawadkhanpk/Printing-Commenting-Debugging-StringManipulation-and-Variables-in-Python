1485ac4c7ce64f2b1e53e18c387e8c2c3143e865
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-22 21:18:15 +0500

    Escape Sequences in Python
    \n - Newline
    \t - Tab
    \\ - Backslash
    \' - Single Quote
    \" - Double Quote
    \b - Backspace
    \r - Carriage Return
    \f - Formfeed
    \ooo - Octal value (where ooo is a three-digit octal number)
    \xhh - Hex value (where hh is a one or two-digit hexadecimal number)

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello world")
+# Newline (\n)
+print("Hello\nWorld")
+
+# Tab (\t)
+print("This is a\tTab")
+
+# Backslash (\\)
+print("C:\\Users\\John")
+
+# Single Quote (\')
+print("I\'m learning Python")
+
+# Double Quote (\")
+print("She said, \"Hello!\"")
+
+# Backspace (\b)
+print("Back\bSpace")
+
+# Carriage Return (\r)
+print("Carriage\rReturn")
+
+# Formfeed (\f)
+print("Page1\fPage2")
+
+# Octal value (\ooo)
+print("\110\145\154\154\157")  # Output: Hello (ASCII values for 'H', 'e', 'l', 'l', 'o')
+
+# Hex value (\xhh)
+print("\x48\x65\x6c\x6c\x6f")  # Output: Hello (Hexadecimal values for 'H', 'e', 'l', 'l', 'o')
+
+
