738968925f95b3774faefcce0cd94e2f4a28c5d8
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-22 21:52:27 +0500

    Taking Input in Python

---
+++ Input-in-Python.py
@@ -0,0 +1,16 @@
+# Taking input from the user
+name = input("Enter your name: ")
+
+# Printing the input
+print("Hello,", name)
+
+
+# To take input and to print using single line of code
+print("Hello "+ input("Enter Your Name"))
+
+
+# # Taking password input securely
+# import getpass
+# password = getpass.getpass(prompt="Enter your password: ")
+# # Printing the input
+# print("Password entered:", password)
